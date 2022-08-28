38da26299d7947dcc0545209922805bb55511dfe
João Victor Batista <jvtexbat@hotmail.com>
2022-08-27 21:29:16 -0300

    test: create HelpRepository test file

---
+++ __tests__/repository/HelpRepository.spec.js
@@ -0,0 +1,15 @@
+const HelpRepository = require("../../src/repository/HelpRepository");
+
+
+const repository = new HelpRepository();
+
+
+describe('#HelpRepository', () => {
+  describe('#getHelpTopLocationsList', () => {
+    it('test to initially fail', () => {
+
+      expect(true).toBe(false);
+    });
+
+  });
+});
